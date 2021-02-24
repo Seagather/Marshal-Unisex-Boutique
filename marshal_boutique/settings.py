efcf5f5b44f68b172378c58cdb3ba2544c46194f
Patrick <gather4real@yahoo.com>
2021-02-24 20:36:44 +0000

    Removed secret key and set debug

--- marshal_boutique/settings.py
+++ marshal_boutique/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4p8n3mudhwtm*5&27!=s$_hp%l%*o(a&a&k=)rb4rgp&(^1diw'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['marshal-unisex-boutique.herokuapp.com', 'localhost']
 
