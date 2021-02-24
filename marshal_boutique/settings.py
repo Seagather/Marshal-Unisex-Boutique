acf1b794dfc3468eaee5137d92e65573bb212651
Patrick <gather4real@yahoo.com>
2021-02-24 18:31:18 +0000

    Heroku Deployment part 2

--- marshal_boutique/settings.py
+++ marshal_boutique/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '4p8n3mudhwtm*5&27!=s$_hp%l%*o(a&a&k=)rb4rgp&(^1diw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['marshal-unisex-boutique.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'marshal_boutique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
